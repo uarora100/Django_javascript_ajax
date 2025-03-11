8f29cd84a9e866b39f3b276e6b0464c232782745
uarora100 <Arorasabb100@gmail.com>
2025-03-11 15:28:37 +0530

    load more ost with a button click

--- src/posts/views.py
+++ src/posts/views.py
@@ -7,20 +7,24 @@ def post_list_and_create(request):
      qs = Post.objects.all()
      return render(request, 'posts/main.html', {'qs':qs})
 
-def load_post_data_view(request):
-    qs = Post.objects.all()
-    # data = serializers.serialize('json', qs)
-    data = []           # make it dictionary
-    for obj in qs:
+def load_post_data_view(request, num_posts):
+        visible = 3
+        upper = num_posts           
+        lower = upper - visible         
+        size = Post.objects.all().count()
+        qs = Post.objects.all()
+        # data = serializers.serialize('json', qs)
+        data = []           # make it dictionary
+        for obj in qs:
             item = {        # take information and put them into dictionary
                 'id' : obj.id,
                 'title' : obj.title,
                 'body' : obj.body,
+                'liked' : True if request.user in obj.liked.all() else False,
                 'author' : obj.author.user.username
             }
             data.append(item)
-    return JsonResponse({'data': data})
-
+        return JsonResponse({'data': data[lower:upper], 'size' : size})
 
 def hello_world_view(request):
      return JsonResponse({'text' : 'hello world x2'})
